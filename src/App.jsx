eadcdc909960fddf24a4a36afb2a8d4db93965fa
prachi1025 <prachipargal123@gmail.com>
2025-03-08 04:32:43 +0530

    add gameWon and Confetti feature
    add confetti when game is won i.e. when all the die have same value and all are held

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
 import React from "react"
 import Die from "./assets/components/Die"
 import {nanoid} from "nanoid"
+import Confetti from "react-confetti"
 
 export default function App() {
+
+  const [windowSize, setWindowSize] = React.useState({
+    width: window.innerWidth,
+    height: window.innerHeight
+  });
+
+  React.useEffect(() => {
+      function handleResize() {
+          setWindowSize({
+              width: window.innerWidth,
+              height: window.innerHeight
+          });
+      }
+
+      window.addEventListener("resize", handleResize);
+
+      // Cleanup function to remove event listener
+      return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+
+  const [randomDie, setRandomDie] = React.useState(generateAllNewDice)
+
+  const gameWon = randomDie.every(die => die.isHeld) && randomDie.every(die => die.value === randomDie[0].value)
 
   function hold(id) {
     setRandomDie(prevRandomDie => 
@@ -23,8 +48,6 @@ export default function App() {
     
     return dieObjectArray
   }
-
-  const [randomDie, setRandomDie] = React.useState(generateAllNewDice)
   
   const dieElements = randomDie.map(dieObject => <Die key={dieObject.id} id={dieObject.id} value={dieObject.value} isHeld={dieObject.isHeld} hold={hold} />)
 
@@ -35,10 +58,15 @@ export default function App() {
 
   return (
     <main>
+      {gameWon && <Confetti width={windowSize.width} height={windowSize.height}/>}
+      <div className="container">
+        <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <section className = "die-container">
         {dieElements}
       </section>
-      <button className="roll" onClick={rollDie}><span>Roll</span></button>
-    </main>
+      <button className="roll" onClick={rollDie}><span>{gameWon ? "New Game": "Roll"}</span></button>
+      </div>
+    </main>  
   )
 }
